1e4655e05c6915687bf698c7b8711aa1b768ce82
Karla Moreno <moreno_karla@live.com>
2017-09-29 11:29:05 -0500

    start ajaxifying reddit!

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ var frontPage = 'https://www.reddit.com/.json';
 
 
 
-
-
 $(document).ready(function(){
 /* FUNCTION EXECUTION HERE */
   console.log('Go forth and code!');
+
+  $.ajax({
+    method: 'GET',
+    url: frontPage,
+    // data: ,
+    dataType: 'json',
+    success: onSuccess,
+    error: onError
+  });
+
+
 
 });
 
@@ -15,3 +24,14 @@ $(document).ready(function(){
 
 /* FUNCTION DEFINITION HERE */
 /* TIP: don't forget scope! */
+
+function onSuccess(response){
+  console.log('success! you did the ajax thing');
+  // test to ensure i'm getting the data (title) from the first 'article'
+  var test = response.data.children[0].data.title;
+  console.log('title: ' + test);
+}
+
+function onError(){
+  console.log('error: seek & destroy, fam. u can do it.');
+}
